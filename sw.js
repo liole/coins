915e6ca10a0ae8379cba80bbf469f8173262aa43
Oleg Pylypchak <liole.plo@gmail.com>
2018-02-02 16:45:19 +0100

    Offline version

---
+++ sw.js
@@ -0,0 +1,49 @@
+var CACHE_NAME = 'offline-cache-v1';
+var urlsToCache = [
+    '/',
+    '/script.js',
+    '/style.css',
+// images
+    '/cloud.png',
+    '/flag.png',
+    '/icon.png',
+    '/refresh.png',
+    '/sync.png',
+    '/upload.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName != CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
